aa28f88b1a9e52fe5c011fd86527952c8a89549e
Ivanq <imachug@yandex.ru>
2018-01-01 13:02:16 +0300

    Guess private key

--- main.py
+++ main.py
@@ -8,9 +8,21 @@ sys.path.insert(0, os.path.join(zeronet_directory, "plugins/CryptMessage"))  # E
 sys.path.insert(0, os.path.join(zeronet_directory, "src/lib"))  # External liblary directory
 sys.path.insert(0, os.path.join(zeronet_directory, "src"))  # Imports relative to src
 
-import zeromail
+import json
 
-zeromail.connect(zeronet_directory, pub="", priv="")
+privatekey = None
+try:
+    with open(zeronet_directory + "data/users.json", "r") as f:
+        users = json.loads(f.read())
+        user = users[users.keys()[0]]
+        zeromail = user["sites"]["1MaiL5gfBM1cyb4a8e3iiL8L5gXmoAJu27"]
+        keyname = [key for key in zeromail.keys() if "encrypt_privatekey" in key][0]
+        privatekey = zeromail[keyname]
+except:
+    privatekey = raw_input("ZeroMail private key:")
+
+import zeromail
+zeromail.connect(zeronet_directory, pub="", priv=privatekey)
 
 print "Loading secrets..."
 secrets = zeromail.get_secrets()
