e1846b9f03a7c0ffec4d2ba321d87446b288b29c
Ivanq <imachug@yandex.ru>
2018-01-01 13:06:12 +0300

    Guess public key

--- main.py
+++ main.py
@@ -10,19 +10,37 @@ sys.path.insert(0, os.path.join(zeronet_directory, "src"))  # Imports relative t
 
 import json
 
+# Guess private key
+zeroid = None
 privatekey = None
 try:
     with open(zeronet_directory + "data/users.json", "r") as f:
         users = json.loads(f.read())
         user = users[users.keys()[0]]
+
+        zeroid = user["certs"]["zeroid.bit"]["auth_address"]
+
         zeromail = user["sites"]["1MaiL5gfBM1cyb4a8e3iiL8L5gXmoAJu27"]
         keyname = [key for key in zeromail.keys() if "encrypt_privatekey" in key][0]
         privatekey = zeromail[keyname]
 except:
+    if zeroid is None:
+        print "Could not load ZeroID address"
+        sys.exit(1)
+
     privatekey = raw_input("ZeroMail private key:")
 
+# Guess public key
+publickey = None
+try:
+    with open(zeronet_directory + "data/1MaiL5gfBM1cyb4a8e3iiL8L5gXmoAJu27/data/users/" + zeroid + "/data.json") as f:
+        data = json.loads(f.read())
+        publickey = data["publickey"]
+except:
+    publickey = raw_input("ZeroMail public key:")
+
 import zeromail
-zeromail.connect(zeronet_directory, pub="", priv=privatekey)
+zeromail.connect(zeronet_directory, pub=publickey, priv=privatekey)
 
 print "Loading secrets..."
 secrets = zeromail.get_secrets()
