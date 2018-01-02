325ec34d0888b2164d3c4e8059684259a4d3e201
Ivanq <imachug@yandex.ru>
2018-01-02 21:13:29 +0300

    Rename pop3.py

--- pop.py
+++ pop.py
@@ -2,8 +2,8 @@ import os
 import socket
 import sys
 
-from server import Server
-from mailbox import Mailbox
+from pop3.server import Server
+from pop3.mailbox import Mailbox
 
 server = Server("localhost", 110, Mailbox=Mailbox)
 server.serve()
