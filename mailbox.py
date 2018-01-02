cdbba48f7e93668e22865648b6494e3f2d777853
Ivanq <imachug@yandex.ru>
2018-01-02 23:10:54 +0300

    Mailbox: Serve ZeroMail

--- mailbox.py
+++ mailbox.py
@@ -1,4 +1,6 @@
+import zeromail
 from pop3.message import Message
+from config import zeronet_directory
 
 MESSAGE = "From: Me <me@zeromail.bit>\r\nTo: Wow <wow@zeromail.bit>\r\nSubject: Hello\r\nDate: Fri, 21 Nov 1997 09:55:06 -0600\r\nMessage-ID: zeromail0\r\n\r\nHello!\r\n"
 
@@ -7,14 +9,21 @@ class Mailbox(object):
 		self.user = user
 		self.password = password
 
-		self.messages = [Message(MESSAGE), Message(MESSAGE), Message(MESSAGE)]
+		zeromail.connect(zeronet_directory, pub=user, priv=password)
+
+	def load_messages(self):
+		secrets = zeromail.update_secrets()
+		messages = zeromail.update_messages(secrets)
+		return messages
 
 	def messageCount(self):
-		return len(self.messages)
+		return len(self.load_messages())
 	def __len__(self):
-		return sum([len(message) for message in self.messages])
+		return sum([len(message) for message in self.load_messages()])
 
 	def getMessageIds(self):
-		return range(1, len(self.messages) + 1)
+		return [str(int(key) % 1000000) for key in self.load_messages().keys()]
 	def __getitem__(self, message):
-		return self.messages[message - 1]
+		messages = self.load_messages()
+		key = [cur for cur in messages if int(cur) % 1000000 == int(message)][0]
+		return messages[key]
