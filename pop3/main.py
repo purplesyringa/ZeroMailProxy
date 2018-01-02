ad8a4f7aec33cac90c47a3ddbeb7892cbb0ddebb
Ivanq <imachug@yandex.ru>
2018-01-02 21:12:01 +0300

    Always server POP3 on port 110

--- pop3/main.py
+++ pop3/main.py
@@ -5,23 +5,5 @@ import sys
 from server import Server
 from mailbox import Mailbox
 
-def serve(host, port):
-    server = Server(host, port, Mailbox=Mailbox)
-    server.serve()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "USAGE: [<host>:]<port>"
-    else:
-        _, port = sys.argv
-        if ":" in port:
-            host = port[:port.index(":")]
-            port = port[port.index(":") + 1:]
-        else:
-            host = ""
-        try:
-            port = int(port)
-        except Exception:
-            print "Unknown port:", port
-        else:
-            serve(host, port)
+server = Server("localhost", 110, Mailbox=Mailbox)
+server.serve()
