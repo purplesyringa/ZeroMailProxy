db9d9e0a3c25825f693f99e7a4087991c845f402
Ivanq <imachug@yandex.ru>
2018-01-03 15:05:51 +0300

    Move SMTP commands to Transaction

--- smtp/session.py
+++ smtp/session.py
@@ -5,8 +5,9 @@ import time
 class Session(object):
 	def __init__(self, conn, Mailbox):
 		self.conn = conn
-		self.state = None
 		self.raw_handler = None
+		self.Mailbox = Mailbox
+		self.transaction = None
 		self.init()
 
 	def sendall(self, *args, **kwargs):
@@ -32,7 +33,9 @@ class Session(object):
 		while True:
 			data = self.recvall()
 
-			if self.raw_handler is None:
+			handler = self.transaction if self.transaction is not None else self
+
+			if handler.raw_handler is None:
 				command = data.split(None)[0]
 				args = data.split(None)[1:]
 
@@ -43,74 +46,22 @@ class Session(object):
 				name = "command" + command[0].upper() + command[1:].lower()
 
 				try:
-					if name in dir(self):
-						getattr(self, name)(*args)
+					if name in dir(handler):
+						getattr(handler, name)(*args)
+					else:
+						self.status(500, "Unknown command")
 				except AssertionError as e:
 					self.status(503, "bad sequence of commands")
 			else:
-				getattr(self, self.raw_handler)(data)
+				getattr(handler, handler.raw_handler)(data)
 
 	def commandEhlo(self, server):
-		assert self.state == "awaitEhlo"
-
-		self.state = "awaitMail"
 		self.ok("SMTP server here")
+		self.transaction = Transaction(self.conn)
 
-	def commandMail(self, *args):
-		assert self.state == "awaitMail"
-
-		args = self.parseColon(args)
-		self.from_ = args["FROM"][1:-1]
-		self.to = []
-		self.data = ""
-
-		self.state = "awaitRcpt"
-		self.ok("Ready to accept recipients")
-
-	def commandRcpt(self, *args):
-		assert self.state == "awaitRcpt"
-
-		args = self.parseColon(args)
-		self.to.append(args["TO"][1:-1])
-
-		self.state = "awaitRcpt"
-		self.ok(args["TO"][1:-1] + " ok")
-
-	def commandData(self, *args):
-		assert self.state == "awaitRcpt"
-		self.raw_handler = "handleData"
-		self.data = ""
-		self.status(354, "Intermediate reply")
-	def handleData(self, data):
-		if data == "":
-			self.data += "\r\n"
-		elif data == ".":
-			self.state = "awaitRcpt"
-			self.send(self.from_, self.to, self.data)
-			self.from_ = ""
-			self.to = ""
-			self.data = ""
-			self.raw_handler = None
-		elif data[0] == ".":
-			self.data += data[1:] + "\r\n"
-		else:
-			self.data += data + "\r\n"
-	def send(self, from_, to, data):
-		self.ok("Message sent")
-
-		print ""
-		print ""
-		print "From: " + from_
-		print "To: " + ", ".join(to)
-		print ""
-		print data
-		print ""
-		print ""
-
-	def parseColon(self, args):
-		res = dict()
-		for arg in args:
-			key = arg.split(":", 1)[0]
-			value = arg.split(":", 1)[1]
-			res[key.upper()] = value
-		return res
+	def commandMail(self):
+		self.status(503, "Auth required")
+	def commandRcpt(self):
+		self.status(503, "Auth required")
+	def commandData(self):
+		self.status(503, "Auth required")
