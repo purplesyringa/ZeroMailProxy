93c67337ab8785f3a1770a73ee78513f5f7e95a7
Ivanq <imachug@yandex.ru>
2018-01-04 15:32:25 +0300

    Move sys.path changning to config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 zeronet_directory = "/home/ivanq/Documents/ZeroNet/" # <-- Change me depending on OS/Package settings
+
+# Load ZeroNet plugins
+os.chdir(zeronet_directory)
+sys.path.insert(0, os.path.join(zeronet_directory, "plugins/CryptMessage"))  # External liblary directory
+sys.path.insert(0, os.path.join(zeronet_directory, "src/lib"))  # External liblary directory
+sys.path.insert(0, os.path.join(zeronet_directory, "src"))  # Imports relative to src
--- pop.py
+++ pop.py
@@ -4,12 +4,6 @@ import sys
 
 from config import zeronet_directory
 
-# Load ZeroNet plugins
-os.chdir(zeronet_directory)
-sys.path.insert(0, os.path.join(zeronet_directory, "plugins/CryptMessage"))  # External liblary directory
-sys.path.insert(0, os.path.join(zeronet_directory, "src/lib"))  # External liblary directory
-sys.path.insert(0, os.path.join(zeronet_directory, "src"))  # Imports relative to src
-
 from pop3.server import Server
 from mailbox import Mailbox
 
